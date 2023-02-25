19cb02a2e1cf44ffeed7d5a6c0db04c46e3834aa
Mayank <hellomayankashok@gmail.com>
2023-02-24 20:14:44 -0800

    add support to send email using aws ses

--- dist/scrapper.js
+++ dist/scrapper.js
@@ -1,27 +1,60 @@
-import fetch from "node-fetch";
 import cheerio from "cheerio";
-
-export const handler = async (event, context) => {
-  const findTextOnPage = async (url, targetText) => {
-    try {
-    const response = await fetch(url);
-    const html = await response.text();
-    const $ = cheerio.load(html);
-    const elementContainingText = $(`*:contains('${targetText}'):last`);
-    const textContent = elementContainingText.text().trim();
-    return textContent;  
-    } catch (error) {
-      console.log({error})
-    }
-    
-  };
-
-  const url =
-    "https://www.canada.ca/en/immigration-refugees-citizenship/services/immigrate-canada/family-sponsorship/sponsor-parents-grandparents/tell-us-you-want-sponsor-parent-grandparent.html";
-
-  const targetText = "We aren’t accepting";
-
-  const answer = await findTextOnPage(url, targetText);
-
-  console.log({ answer });
+import fetch from "node-fetch";
+import AWS from "aws-sdk";
+AWS.config.update({
+    accessKeyId: 'AKIA42ORE75XEKK2LXDY',
+    secretAccessKey: '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',
+    region: 'us-west-2'
+});
+const SES = new AWS.SES();
+const sendEmail = async (recipient, subject, body, source) => {
+    const params = {
+        Destination: {
+            ToAddresses: [recipient],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: body,
+                },
+            },
+            Subject: {
+                Data: subject,
+            },
+        },
+        Source: source,
+    };
+    await SES.sendEmail(params).promise();
 };
+export const findTextOnPage = async (url, targetText, recipient, source) => {
+    try {
+        const res = await fetch(url);
+        const html = await res.text();
+        const $ = cheerio.load(html);
+        const elementContainingText = $(`*:contains('${targetText}'):last`);
+        const textContent = elementContainingText.text().trim();
+        console.log({ textContent });
+        if (textContent) {
+            // Send an email using AWS SES
+            const body = `IRCC: We are NOT accepting parents PR application.  Visit to confirm: ${url}.`;
+            await sendEmail(recipient, "IRCC: We are not accepting parents PR application. ", body, source);
+            return `Target text found on ${url}.`;
+        }
+        else {
+            const body = `CONGRATULATIONS: WE ARE NOW ACCEPTING parents PR application.  Visit to confirm: ${url}.`;
+            await sendEmail(recipient, "URGENT IRCC: WE ARE NOW ACCEPTINGaccepting parents PR application. ", body, source);
+            return `Target text not found on ${url}.`;
+        }
+    }
+    catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+// Example usage:
+const url = "https://www.canada.ca/en/immigration-refugees-citizenship/services/immigrate-canada/family-sponsorship/sponsor-parents-grandparents/tell-us-you-want-sponsor-parent-grandparent.html";
+const targetText = "We aren’t accepting";
+const recipient = "hellomayankashok@gmail.com";
+const source = "hellomayankashok@gmail.com";
+const answer = await findTextOnPage(url, targetText, recipient, source);
+console.log({ answer });
--- scrapper.ts
+++ scrapper.ts
@@ -1,12 +1,75 @@
-import fetch from 'node-fetch';
-import cheerio from 'cheerio';
+import cheerio from "cheerio";
+import fetch from "node-fetch";
+import AWS from "aws-sdk";
 
-export async function findTextOnPage(url: string, text: string): Promise<boolean> {
-  const response = await fetch(url);
-  const body = await response.text();
+AWS.config.update({
+  accessKeyId: 'AKIA42ORE75XEKK2LXDY',
+  secretAccessKey: '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',
+  region: 'us-west-2'
+});
 
-  const $ = cheerio.load(body);
-  const found = $('body:contains("' + text + '")').length > 0;
 
-  return found;
-}
+const SES = new AWS.SES();
+
+const sendEmail = async (recipient: string, subject: string, body: string, source: string) => {
+  const params = {
+    Destination: {
+      ToAddresses: [recipient],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: body,
+        },
+      },
+      Subject: {
+        Data: subject,
+      },
+    },
+    Source: source,
+  };
+  await SES.sendEmail(params).promise();
+};
+
+export const findTextOnPage = async (
+  url: string,
+  targetText: string,
+  recipient: string,
+  source: string
+): Promise<string> => {
+  try {
+    const res = await fetch(url);
+    const html = await res.text();
+    const $ = cheerio.load(html);
+    const elementContainingText = $(`*:contains('${targetText}'):last`);
+    const textContent = elementContainingText.text().trim();
+
+    console.log({ textContent})
+
+    if (textContent) {
+      // Send an email using AWS SES
+      const body = `IRCC: We are NOT accepting parents PR application.  Visit to confirm: ${url}.`;
+      await sendEmail(recipient, "IRCC: We are not accepting parents PR application. ", body, source);
+      return `Target text found on ${url}.`;
+    } else {
+      const body = `CONGRATULATIONS: WE ARE NOW ACCEPTING parents PR application.  Visit to confirm: ${url}.`;
+      await sendEmail(recipient, "URGENT IRCC: WE ARE NOW ACCEPTINGaccepting parents PR application. ", body, source);
+      return `Target text not found on ${url}.`;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// Example usage:
+const url =
+  "https://www.canada.ca/en/immigration-refugees-citizenship/services/immigrate-canada/family-sponsorship/sponsor-parents-grandparents/tell-us-you-want-sponsor-parent-grandparent.html";
+
+const targetText = "We aren’t accepting";
+const recipient = "hellomayankashok@gmail.com";
+const source = "hellomayankashok@gmail.com";
+
+const answer = await findTextOnPage(url, targetText, recipient, source);
+
+console.log({ answer });
